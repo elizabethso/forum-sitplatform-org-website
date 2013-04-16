9c848b4f2478ea49bfd17378e1cac6178d683f76
douglas.harley@esacinc.com <douglas.harley@esacinc.com@ccd290a1-b46b-20c2-3ab8-f1cbda67a867>
2013-04-16 20:09:52 +0000

    adding phpBB to source control

---
+++ trunk/phpBB3/config.php
@@ -0,0 +1,18 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = 'localhost';
+$dbport = '3306';
+//$dbname = 'phpBB3_db';
+$dbname = 'phpbb3';
+$dbuser = 'phpBB3_user';
+$dbpasswd = 'phpBB3_user_pass!';
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+
